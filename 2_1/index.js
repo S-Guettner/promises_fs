8470c0715971c7d3d09f6dd95f561bff6363571e
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-03-17 15:24:32 +0100

    f

--- 2_1/index.js
+++ 2_1/index.js
@@ -7,7 +7,7 @@ import { dataFile } from './data.js'
 const readFile = (dataInput) => {
     return new Promise((resolve,reject) => {
         dataInput.forEach((item) => {
-            fs.writeFile('./data.txt',item.id + item.title + item.description + '\n \n' ,{flag : 'a+'}, (err) => {
+            fs.writeFileSync('./data.txt',item.id + item.title + item.description + '\n \n' ,{flag : 'a+'}, (err) => {
                 if (err) reject(err)
                 else {
                     resolve("Daten erstellt")
@@ -16,7 +16,6 @@ const readFile = (dataInput) => {
         })
     })
 }
-
 
 
 readFile(dataFile)
